4cdce24aefe406fc59e315302957f732b915d3e9
Kevin <kevin1061517@gmail.com>
2023-01-17 23:24:07 +0800

    add some word

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -30,19 +30,10 @@ export function Home() {
                 </div>
                 <div className="message-list">
                     <div className="message-item message-from-other">
-                        <span>對啊</span>
-                    </div>
-                    <div className="message-item message-from-other">
-                        <span>試著</span>
-                    </div>
-                    <div className="message-item message-from-other">
-                        <span>靠左邊嘛</span>
+                        <span>已建立連線</span>
                     </div>
                     <div className="message-item message-from-me">
-                        <span>換我了</span>
-                    </div>
-                    <div className="message-item message-from-me">
-                        <span>有看到嗎</span>
+                        <span>開始說你好...</span>
                     </div>
                 </div>
                 <div className="footer">
